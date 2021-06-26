1014147af131afb127151626359cb7976266d5b2
npaterno <nicholas.paterno@gmail.com>
2021-06-26 13:02:00 -0700

    new image

--- experiments_2.R
+++ experiments_2.R
@@ -12,3 +12,15 @@ seq(-10,10,0.05) %>%
   guides(color = FALSE)
 
 ggsave(here::here("images/conic_wave_twitter.png"))
+
+seq(-10, 10, 0.02) %>% 
+  expand.grid(x = ., y= .) %>% 
+  ggplot()+
+  geom_point(aes(x = y^2+x^2-x-y, y = x*sin(y), color = y^2+x),
+             alpha = 0.01, shape = 20, size = 0)+
+  geom_point(aes(x =-y^2-x^2+x+y, y = x*sin(y), color = x^2+y),
+             alpha = 0.01, shape = 20, size = 0)+
+  scale_color_viridis_c(option = "turbo") +
+  dark_theme_void()+
+  #coord_polar()+
+  guides(color = FALSE)
