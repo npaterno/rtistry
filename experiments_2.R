4f866f7cb5c1e205de0b587a455c63bf2b5fb17d
npaterno <nicholas.paterno@gmail.com>
2021-07-09 09:25:28 -0700

    minor change

--- experiments_2.R
+++ experiments_2.R
@@ -13,14 +13,14 @@ seq(-10,10,0.05) %>%
 
 ggsave(here::here("images/conic_wave_twitter.png"))
 
-seq(-10, 10, 0.02) %>% 
+seq(-10, 10, 0.025) %>% 
   expand.grid(x = ., y= .) %>% 
   ggplot()+
-  geom_point(aes(x = y^2+x^2-x-y, y = x*sin(y), color = y^2+x),
-             alpha = 0.01, shape = 20, size = 0)+
-  geom_point(aes(x =-y^2-x^2+x+y, y = x*sin(y), color = x^2+y),
-             alpha = 0.01, shape = 20, size = 0)+
+  geom_point(aes(x = y^2+y^2-y-y, y = x*sin(x), color = y^2+x),
+             alpha = 0.02, shape = 20, size = 0)+
+  geom_point(aes(x =-x^2-x^2+x+x, y = y*cos(y), color = x^2+y),
+             alpha = 0.02, shape = 20, size = 0)+
   scale_color_viridis_c(option = "turbo") +
   dark_theme_void()+
-  #coord_polar()+
+  coord_polar()+
   guides(color = FALSE)
