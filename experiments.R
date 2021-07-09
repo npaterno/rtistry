7518607d8f8ac07a92de423900a41c1e5ea19f37
npaterno <nicholas.paterno@gmail.com>
2021-07-09 13:53:44 -0700

    added b&w polar_2

--- experiments.R
+++ experiments.R
@@ -22,20 +22,20 @@ plot+coord_polar()
 ggsave(here::here("images/polar_1.png"))
 
 # round 2
-plot <- flametree_grow(seed = 404, 
-                       time = 5, 
+plot <- flametree_grow(seed = 808, 
+                       time = 8, 
                        scale = c(0.25, 0.50, 0.75, 1.0, 1.5),
                        angle = c(-30, -15, 10, 20),
-                       split = 8,
-                       trees = 2,
-                       shift_x = spark_linear(),
-                       shift_y = spark_random()
+                       split = 4,
+                       trees = 3,
+                       shift_x = spark_random(constant = 1),
+                       shift_y = spark_random(constant = 2)
 ) %>% 
   flametree_plot(
-    palette = c("cadetblue",
-                "blue",
-                "cyan",
-                "darkslateblue")
+    palette = c("snow1",
+                "snow2",
+                "snow3",
+                "snow4")
   )
 
 plot+coord_polar()
