90d67dcfeab599871f4180c95933ff9bfa10a6ce
npaterno <nicholas.paterno@gmail.com>
2021-09-17 22:12:18 -0700

    success!

--- kelly.R
+++ kelly.R
@@ -2,19 +2,18 @@
 library(ggplot2)
 library(dplyr)
 
-# Generate random data frame
+# Generate data frame with random values for color
+color_data <- sample(1:18, 1600, replace = TRUE)
 df <- data.frame(expand.grid(0:39, 0:39))
+plot_data <- df %>% 
+  mutate(color_data = as.integer(color_data))
 
 # Generate colors
 clrs = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
 plot_clrs = sample(clrs, 18)
 
 
-# Test plot
-kelly <- function(first, second){
-  ggplot(df)+   
-  geom_rect(aes(xmin = first, xmax = first+1, ymin = second, ymax = second+1), fill = plot_clrs[runif(1, min=1, max=18)])
-}
-
-purrr::pwalk(expand.grid(1:2,1:2),~kelly(.x, .y))
-
+# Generate Plot
+ggplot(plot_data) +
+  geom_rect(aes(xmin = Var1, xmax = Var1+1, ymin = Var2, ymax = Var2+1), fill = plot_clrs[color_data])+
+  theme_void()
